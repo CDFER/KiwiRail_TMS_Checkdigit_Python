817c277807e12034ed735f694c86a81e8768eccf
CDFER <cdirksfer@gamil.com>
2025-01-01 12:34:04 +1300

    setup for pip and add readme

--- src/KiwiRail_TMS_Checkdigit/__init__.py
+++ src/KiwiRail_TMS_Checkdigit/__init__.py
@@ -5,6 +5,6 @@ A package for calculating the check digit of a KiwiRail TMS number
 __version__ = "1.0.0"
 
 from .tms_checkdigit import (
-    convert_char_to_digits,
+    is_check_digit_valid,
     calculate_check_digit,
 )
